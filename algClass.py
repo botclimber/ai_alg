53fc43d4a51541889d151e716904e418ee4a8c77
Anonymous <anon@Anonymouss-MacBook-Air.local>
2020-10-23 14:54:30 +0100

    setting it up

--- algClass.py
+++ algClass.py
@@ -1,7 +1,23 @@
 # GENERIC CLASS
 
-class magicBox:
-	
-	def __init__(self, name):
-		print(name)
-	
+# INITIAL STATE
+# FINAL STATE
+# ALL STATES
+# TARGET TEST (COMPARE ACTUAL STATE WITH TARGET STATE)
+# ACTIONS
+
+
+class magicBox:	
+
+	def __init__(self, iState, fState = 0):
+		
+		self.iState = iState
+		self.fState = fState # optional
+
+	def chState(self):
+		# METHOD TO MOVE INTO NEXT STATE
+		return		
+
+	def whoami(self):
+		print(self.iState)
+		print(self.fState)
--- example_1.py
+++ example_1.py
@@ -1,4 +1,5 @@
 from algClass import magicBox
 
-x = magicBox('Daniel Silva')
+x = magicBox( [[1,2,3],[4,5,6],[7,8,9]], [[3,2,1],[6,5,4],[9,8,7]])
+x.whoami()
 
